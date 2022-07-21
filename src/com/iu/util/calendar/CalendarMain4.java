eb13b53f49630877f2c211d2aa94a78fb86a95ea
KOOHYO <zpfhzpfh112@naver.com>
2022-07-21 17:47:52 +0900

    캘린더와 IO

--- src/com/iu/util/calendar/CalendarMain4.java
+++ src/com/iu/util/calendar/CalendarMain4.java
@@ -1,5 +1,6 @@
 package com.iu.util.calendar;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class CalendarMain4 {
@@ -11,6 +12,13 @@ public class CalendarMain4 {
 		//ca.roll(Calendar.MINUTE, 20); //amount 양을 얘기한다
 		ca.add(Calendar.MINUTE, 20);
 		System.out.println(ca.getTime());
+		
+		System.out.println("=========================");
+		ca = Calendar.getInstance();
+		String pattern = "yyyy년 MM월 dd일 E";
+		SimpleDateFormat sd = new SimpleDateFormat(pattern);
+		String time = sd.format(ca.getTime());//데이터를 패턴에 맞는 문자열로 변환!
+		System.out.println(time);
 
 	}
 
